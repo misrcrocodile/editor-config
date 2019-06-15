fc996264d2f38a0c6a21862f466207d599a203da
misscrocodile <misr.crocodile@gmail.com>
2019-06-15 12:31:39 +0900

    edit vscode extension list

---
+++ vs-code/vscode.sh
@@ -0,0 +1,116 @@
+# code --list-extensions | xargs -L 1 echo code --install-extension >> file
+
+# Material Icon Theme
+code --install-extension PKief.material-icon-theme
+
+# IntelliSense for CSS class names in HTML
+code --install-extension Zignd.html-css-class-completion
+
+# HTML Snippets: Full HTML tags including HTML5 Snippets
+code --install-extension abusaidm.html-snippets
+
+# One Monokai Theme: A cross between Monokai and One Dark theme
+code --install-extension azemoh.one-monokai
+
+# Git History: View git log, file history, compare branches or commits
+code --install-extension donjayamanne.githistory
+
+# GitLens — Git supercharged: Supercharge the Git capabilities built into Visual Studio Code
+code --install-extension eamodio.gitlens
+
+# npm: npm support for VS Code
+code --install-extension eg2.vscode-npm-script
+
+# JSON Tools: Tools for manipulating JSON
+code --install-extension eriklynd.json-tools
+
+# Prettier - Code formatter: VS Code plugin for prettier/prettier
+code --install-extension esbenp.prettier-vscode
+
+# XML Formatter: A simple XML formatter for VS Code.
+code --install-extension fabianlauer.vs-code-xml-format
+
+# Code Runner
+# Run C, C++, Java, JS, PHP, Python, Perl, Ruby, Go, Lua, 
+# Groovy, PowerShell, CMD, BASH, F#, C#, VBScript, TypeScript, 
+# CoffeeScript, Scala, Swift, Julia, Crystal, OCaml, R, AppleScript, 
+# Elixir, VB.NET, Clojure, Haxe, Obj-C, Rust, Racket, AutoHotkey, AutoIt, 
+# Kotlin, Dart, Pascal, Haskell, Nim, D, Lisp, Kit
+code --install-extension formulahendry.code-runner
+
+# Zeus Z80 Assembly: Zeus Z80 for ZX Spectrum 48k support for Visual Studio Code
+code --install-extension jia3ep.zeus-z80-asm
+
+# React Native Snippet: React Native, Typescript React Native, StyleSheet, Redux Snippet
+code --install-extension jundat95.react-native-snippet
+
+# Python
+# Linting, Debugging (multi-threaded, remote), Intellisense, code formatting,
+# refactoring, unit tests, snippets, and more.
+code --install-extension ms-python.python
+
+# C/C++: C/C++ IntelliSense, debugging, and code browsing.
+code --install-extension ms-vscode.cpptools
+
+# Node Debug: Node.js debugging support
+code --install-extension ms-vscode.node-debug2
+
+# PowerShell: Develop PowerShell scripts in Visual Studio Code!
+code --install-extension ms-vscode.PowerShell
+
+# Debugger for Chrome 
+# Debug your JavaScript code in the Chrome browser, or any other target 
+# that supports the Chrome Debugger protocol.
+code --install-extension msjsdiag.debugger-for-chrome
+
+# advanced-new-file: Create files anywhere in your workspace from the keyboard
+code --install-extension patbenatar.advanced-new-file
+
+# hexdump for VSCode: Display a specified file in hexadecimal
+code --install-extension slevesque.vscode-hexdump
+
+# TODO Highlight: highlight TODOs, FIXMEs, and any keywords, annotations...
+code --install-extension wayou.vscode-todo-highlight
+
+# Import Cost: Display import/require package size in the editor
+code --install-extension wix.vscode-import-cost
+
+# Markdown All in One
+# All you need to write Markdown (keyboard shortcuts, table of contents, 
+# auto preview and more)
+code --install-extension yzhang.markdown-all-in-one
+
+# SSH FS: File system provider using SSH
+code --install-extension Kelvin.vscode-sshfs
+
+# SFTP: SFTP/FTP sync
+code --install-extension liximomo.sftp
+
+################################################
+# DISABLED EXTENSION
+################################################
+
+# Jekyll Syntax Support: Jekyll Syntax Highlighting for Visual Studio Code
+# code --install-extension ginfuru.ginfuru-vscode-jekyll-syntax
+# code --install-extension ginfuru.vscode-jekyll-snippets
+
+# HTML CSS Support: CSS support for HTML documents
+# code --install-extension ecmel.vscode-html-css
+
+# ESLint: Integrates ESLint JavaScript into VS Code.
+# code --install-extension dbaeumer.vscode-eslint
+
+# XML Tools: XML Formatting, XQuery, and XPath Tools for Visual Studio Code
+# code --install-extension DotJoshJohnson.xml
+
+# Vim: Vim emulation for Visual Studio Code
+# code --install-extension vscodevim.vim
+
+# PHP IntelliSense: Advanced Autocompletion and Refactoring support for PHP
+# code --install-extension felixfbecker.php-intellisense
+
+
+# JS-CSS-HTML Formatter 
+# Format ,prettify and beautify JS, CSS, HTML code by using shortcuts, 
+# context menu or CLI
+# code --install-extension lonefy.vscode-JS-CSS-HTML-formatter
